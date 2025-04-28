a9c01344b877510d5ec45a78b1a08838683c6da5
Benjamin Palko <benjaminpalko@hotmail.com>
2025-04-27 22:49:46 -0400

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 <div class="h-screen w-screen p-12">
 	<div class="bg-base-200 flex min-h-full flex-col rounded-4xl p-8">
 		<Navbar title="Benjamin Palko" {pages} route={page.route} />
-		<div class="flex flex-1 items-stretch p-8">{@render children()}</div>
+		<div class="flex flex-1 items-stretch justify-center p-8">{@render children()}</div>
 		<div>
 			<Footer links={socialLinks} />
 		</div>
