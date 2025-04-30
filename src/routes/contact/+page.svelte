86805522085a21d6c89b6a0a67ccc50ca514d454
Benjamin Palko <benjaminpalko@hotmail.com>
2025-04-30 16:51:46 -0400

    implemented
    contact
    
    site formatting
    
    nodemailer
    
    docker

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,54 +1,63 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { CircleCheck, CircleX, Croissant, Mail, UserRound } from 'lucide-svelte';
+	import {
+		CircleCheck,
+		CircleX,
+		Croissant,
+		Mail,
+		Send,
+		TriangleAlert,
+		UserRound
+	} from 'lucide-svelte';
 	import type { PageProps } from './$types';
 
-	let { form, data }: PageProps = $props();
+	let { form }: PageProps = $props();
 </script>
 
-<form class="w-full" id="contact" method="post" use:enhance>
-	<div class="grid w-full max-w-xl grid-cols-2 gap-6">
-		<div class="col-span-2 flex flex-col items-center">
-			<span class="animate-bounce text-2xl">😁</span>
-			<h2 class="text-2xl">Hello there friend</h2>
+<form id="contact" method="post" use:enhance>
+	<div class="mx-auto grid max-w-3xl grid-flow-col grid-cols-3 grid-rows-7 content-end gap-4 py-8">
+		<div class="col-span-full">
+			{#if form?.response?.rejected.length ?? 0 > 0}
+				<div role="alert" class="alert alert-error w-full">
+					<CircleX />
+					<span>Ruh roh raggy!</span>
+				</div>
+			{:else if form?.response?.accepted.length ?? 0 > 0}
+				<div role="alert" class="alert alert-success w-full">
+					<CircleCheck />
+					<span>Your message has been sent successfully!</span>
+				</div>
+			{:else if form?.incorrect}
+				<div role="alert" class="alert alert-warning w-full">
+					<TriangleAlert />
+					<span>Ruh roh raggy!</span>
+				</div>
+			{/if}
 		</div>
 
-		<h3 class="col-span-2 text-xl">Reach out! 🤝</h3>
+		<h2 class="col-span-full text-3xl font-light">Reach out! 🤝</h2>
 
-		{#if form?.response.rejected.length ?? 0 > 0}
-			<div role="alert" class="alert alert-error col-span-2">
-				<CircleX />
-				<span>Ruh roh raggy!</span>
-			</div>
-		{:else if form?.response.accepted.length ?? 0 > 0}
-			<div role="alert" class="alert alert-success col-span-2">
-				<CircleCheck />
-				<span>Your message has been sent successfully!</span>
-			</div>
-		{/if}
+		<label class="input input-bordered flex w-full items-center">
+			<UserRound />
+			<input name="name" type="text" class="grow" placeholder="Full Name" />
+		</label>
+		<label class="input input-bordered flex w-full items-center gap-2">
+			<Mail />
+			<input name="email" type="text" class="grow" placeholder="Your Email" />
+		</label>
+		<label class="input input-bordered flex w-full items-center gap-2">
+			<Croissant />
+			<input name="subject" type="text" class="grow" placeholder="Subject" />
+		</label>
+		<button type="submit" class="btn btn-primary">Send Message <Send size={20} /></button>
 
-		<div class="flex h-full flex-col gap-4">
-			<label class="input input-bordered flex items-center gap-2">
-				<UserRound />
-				<input name="name" type="text" class="grow" placeholder="Name" />
-			</label>
-			<label class="input input-bordered flex items-center gap-2">
-				<Mail />
-				<input name="email" type="text" class="grow" placeholder="Your Email" />
-			</label>
-			<label class="input input-bordered flex items-center gap-2">
-				<Croissant />
-				<input name="subject" type="text" class="grow" placeholder="Subject" />
-			</label>
+		<div class="col-span-2 row-span-4">
+			<textarea
+				name="message"
+				form="contact"
+				class="textarea textarea-bordered h-full w-full resize-none"
+				placeholder="Message"
+			></textarea>
 		</div>
-
-		<textarea
-			name="message"
-			form="contact"
-			class="textarea textarea-bordered h-full min-w-xl resize-none"
-			placeholder="Message"
-		></textarea>
-
-		<button type="submit" class="btn btn-primary">Send Message</button>
 	</div>
 </form>
