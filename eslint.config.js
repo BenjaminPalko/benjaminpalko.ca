b2844e8d76766a9117771f92a60c89912c0cb17a
Benjamin Palko <baobeld@noreply.localhost>
2025-05-03 23:35:06 -0400

    strapi (#3)
    Co-authored-by: Benjamin Palko <benjaminpalko@hotmail.com>
    Reviewed-on: https://git.palko.ca/baobeld/benjaminpalko/pulls/3

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,9 @@ import ts from 'typescript-eslint';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
+	{
+		ignores: ['./strapi']
+	},
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
