fc5f3e9b08e50cbe87aa758718287ffdb0ed40a0
Benjamin Palko <benjaminpalko@hotmail.com>
2025-06-18 10:13:30 -0400

    fix undefined check

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,19 +7,19 @@
 	let posts = $derived(data.posts);
 </script>
 
-{#if posts.length === 0}
+{#if !posts || posts.length === 0}
 	<h3 class="text-center text-lg font-semibold">Looks like I haven't posted anything yet!</h3>
+{:else}
+	<div class="mx-auto grid gap-8 px-3 py-3 sm:px-8 sm:py-4 lg:grid-cols-2">
+		{#each posts as post (post.id)}
+			<BlogCard
+				id={post.id}
+				img={post.image.url}
+				img-alt={post.image.alt}
+				title={post.title}
+				published={post.published}
+				topics={post.topics}
+			/>
+		{/each}
+	</div>
 {/if}
-
-<div class="mx-auto grid gap-8 px-3 py-3 sm:px-8 sm:py-4 lg:grid-cols-2">
-	{#each posts as post (post.id)}
-		<BlogCard
-			id={post.id}
-			img={post.image.url}
-			img-alt={post.image.alt}
-			title={post.title}
-			published={post.published}
-			topics={post.topics}
-		/>
-	{/each}
-</div>
