570f5dde1d876d16854c24c9af670b7f5f78ece4
Benjamin Palko <benjaminpalko@hotmail.com>
2025-06-20 11:52:51 -0400

    adjust heading spacing

--- src/lib/markdown/renderers/Heading.svelte
+++ src/lib/markdown/renderers/Heading.svelte
@@ -8,15 +8,15 @@
 </script>
 
 {#if depth === 1}
-	<h1 class="mb-4 text-5xl font-bold underline">{text}</h1>
+	<h1 class="my-3 text-4xl font-bold">{text}</h1>
 {:else if depth === 2}
-	<h2 class="mb-3 text-4xl font-bold underline">{text}</h2>
+	<h2 class="my-2 text-3xl font-bold">{text}</h2>
 {:else if depth === 3}
-	<h3 class="mb-2 text-3xl font-semibold underline">{text}</h3>
+	<h3 class="my-2 text-2xl font-semibold">{text}</h3>
 {:else if depth === 4}
-	<h4 class="mb-1 text-2xl font-semibold underline">{text}</h4>
+	<h4 class="my-1 text-xl font-semibold">{text}</h4>
 {:else if depth === 5}
-	<h5 class="mb-0.5 text-xl font-medium underline">{text}</h5>
+	<h5 class="my-1 text-lg font-medium underline">{text}</h5>
 {:else if depth === 6}
-	<h6 class="mb-0.5 text-lg font-medium underline">{text}</h6>
+	<h6 class="my-1 font-medium underline">{text}</h6>
 {/if}
