7b0880b53b0d92e5ab73c1d6ffceb4c496f1a291
Benjamin Palko <benjaminpalko@hotmail.com>
2026-01-22 23:56:48 -0500

    remove strapi

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,18 +1,14 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 
-import type { Strapi } from '$lib/server/strapi';
-
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			strapi: Strapi;
-		}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
-export {};
+export { };
