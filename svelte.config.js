aee09a2c77d5a3b830ea3b1447cb3ae32548f565
Benjamin Palko <benjaminpalko@hotmail.com>
2026-01-27 10:03:31 -0500

    remove console log

--- svelte.config.js
+++ svelte.config.js
@@ -5,28 +5,31 @@ import { join } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex({
-    extensions: ['.md'], layout: {
-      blog: join(import.meta.dirname, './src/lib/layout/Blog.svelte')
-    },
-    highlight: {
-      highlighter: (code, lang, meta) => {
-        console.log(meta);
-        return `<Components.pre code={\`${code}\`} lang={\`${lang}\`} />`;
-      }
-    }
-  })],
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: {
+				blog: join(import.meta.dirname, './src/lib/layout/Blog.svelte')
+			},
+			highlight: {
+				highlighter: (code, lang) => {
+					return `<Components.pre code={\`${code}\`} lang={\`${lang}\`} />`;
+				}
+			}
+		})
+	],
 
-  kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter()
-  },
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	},
 
-  extensions: ['.svelte', '.md']
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
